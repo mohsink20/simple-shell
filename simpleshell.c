3b87ae1b1ddf17fd241794c7378b6e39a2697a70
shu <sshuytbusiness@gmail.com>
2023-11-21 17:03:05 +0700

    1,2,3,4,5,6,8,10,11 working

--- simpleshell.c
+++ simpleshell.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <glob.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <signal.h>
 
 #define MAX_INPUT_SIZE 1024
 #define MAX_TOKENS 100
+#define MAX_HISTORY 100
 
 char prompt[MAX_INPUT_SIZE] = "meowsh😺$ ";
+
+char* history[MAX_HISTORY];
+int history_count = 0;
 
 // Signal handler function for SIGTSTP (CTRL-Z)
 void sigtstp_handler(int signo) {
@@ -22,6 +28,80 @@ void sigint_handler(int signo) {
 
 // Signal handler function for SIGQUIT (CTRL-\)
 void sigquit_handler(int signo) {
+}
+
+void expand_wildcards(char** tokens) {
+    char* expanded_tokens[MAX_TOKENS];
+    int expanded_index = 0;
+
+    for (int i = 0; tokens[i] != NULL; i++) {
+        if (strpbrk(tokens[i], "*?") != NULL) {
+            glob_t results;
+            glob(tokens[i], GLOB_TILDE, NULL, &results);
+            for (int j = 0; j < results.gl_pathc; j++) {
+                expanded_tokens[expanded_index++] = strdup(results.gl_pathv[j]);
+            }
+
+            globfree(&results);
+        } else {
+            expanded_tokens[expanded_index++] = strdup(tokens[i]);
+        }
+    }
+    for (int i = 0; i < expanded_index; i++) {
+        tokens[i] = expanded_tokens[i];
+    }
+    tokens[expanded_index] = NULL;
+}
+// Function to add a command to history
+void add_to_history(const char *command) {
+    if (history_count < MAX_HISTORY) {
+        history[history_count++] = strdup(command);
+    } else {
+        free(history[0]);
+        for (int i = 0; i < MAX_HISTORY - 1; i++) {
+            history[i] = history[i + 1];
+        }
+        history[MAX_HISTORY - 1] = strdup(command);
+    }
+}
+
+// Function to print command history
+void print_history() {
+    for (int i = 0; i < history_count; i++) {
+        printf("%d: %s\n", i + 1, history[i]);
+    }
+}
+
+// Function to get command from history by number
+char* get_command_from_history(int command_number) {
+    if (command_number > 0 && command_number <= history_count) {
+        return strdup(history[command_number - 1]);
+    }
+    return NULL;
+}
+
+// Function to handle command history recall
+char* handle_history_recall(char* input) {
+    int command_number = atoi(input + 1);
+    if (command_number > 0) {
+        char* recalled_command = get_command_from_history(command_number);
+        if (recalled_command != NULL) {
+            return recalled_command;
+        } else {
+            printf("Command not found in history\n");
+            return NULL;
+        }
+    } else if (strcmp(input, "!!") == 0) {
+        if (history_count > 0) {
+            return strdup(history[history_count - 1]);
+        } else {
+            printf("No command in history\n");
+            return NULL;
+        }
+    } else {
+        printf("Invalid history command\n");
+        return NULL;
+    }
 }
 
 char** tokenize(char* line) {
@@ -39,11 +119,7 @@ char** tokenize(char* line) {
 
 void change_prompt(const char* newprompt) {
     if (newprompt != NULL) {
-        if (strstr(newprompt, " ") != NULL) {
-            snprintf(prompt, sizeof(prompt), "%s", newprompt);
-        } else {
-            snprintf(prompt, sizeof(prompt), "%s", newprompt);
-        }
+        snprintf(prompt, sizeof(prompt), "%s", newprompt);
     }
 }
 
@@ -75,17 +151,61 @@ int execute(char** tokens) {
         }
         return 1;
     } else {
-        // Check for pipeline
-        int pipe_index = -1;
-        for (int i = 0; tokens[i] != NULL; i++) {
-            if (strcmp(tokens[i], "|") == 0) {
-                pipe_index = i;
+        int background = 0;
+        int sequential = 0;
+        int i;
+
+        for (i = 0; tokens[i] != NULL; i++) {
+            if (strcmp(tokens[i], "&") == 0) {
+                background = 1;
+                tokens[i] = NULL;
+                break;
+            } else if (strcmp(tokens[i], ";") == 0) {
+                sequential = 1;
+                tokens[i] = NULL;
                 break;
             }
         }
 
+        int pipe_index = -1;
+        for (int j = 0; tokens[j] != NULL; j++) {
+            if (strcmp(tokens[j], "|") == 0) {
+                pipe_index = j;
+                break;
+            }
+        }
+        expand_wildcards(tokens);
+
+         if (tokens[0][0] == '!') {
+            int command_number = atoi(tokens[0] + 1);
+            if (command_number > 0) {
+                char* recalled_command = get_command_from_history(command_number);
+                if (recalled_command != NULL) {
+                    tokens = tokenize(recalled_command);
+                    free(recalled_command);
+                } else {
+                    printf("Command not found in history\n");
+                    return 1;
+                }
+            } else if (strcmp(tokens[0], "!!") == 0) {
+                if (history_count > 0) {
+                    char* last_command = strdup(history[history_count - 1]);
+                    tokens = tokenize(last_command);
+                    free(last_command);
+                } else {
+                    printf("No command in history\n");
+                    return 1;
+                }
+            } else {
+                printf("Invalid history command\n");
+                return 1;
+            }
+        }
+
+        add_to_history(tokens[0]);
+
+
         if (pipe_index != -1) {
-            // Pipeline
             char** first_command = tokens;
             char** second_command = tokens + pipe_index + 1;
             tokens[pipe_index] = NULL;
@@ -99,34 +219,28 @@ int execute(char** tokens) {
             pid_t pid = fork();
 
             if (pid == 0) {
-                // Child process (left side of the pipe)
                 close(pipe_fd[0]);
                 dup2(pipe_fd[1], STDOUT_FILENO);
                 close(pipe_fd[1]);
 
-                // Execute the first command
                 execvp(first_command[0], first_command);
                 perror("execvp");
                 exit(1);
             } else if (pid > 0) {
-                // Parent process
                 wait(NULL);
                 close(pipe_fd[1]);
 
                 pid_t pid2 = fork();
 
                 if (pid2 == 0) {
-                    // Child process (right side of the pipe)
                     close(pipe_fd[1]);
                     dup2(pipe_fd[0], STDIN_FILENO);
                     close(pipe_fd[0]);
 
-                    // Execute the second command
                     execvp(second_command[0], second_command);
                     perror("execvp");
                     exit(1);
                 } else if (pid2 > 0) {
-                    // Parent process
                     close(pipe_fd[0]);
                     wait(NULL);
                 } else {
@@ -138,7 +252,6 @@ int execute(char** tokens) {
                 exit(1);
             }
         } else {
-            // No pipeline
             glob_t results;
             int stat = glob(tokens[0], GLOB_TILDE, NULL, &results);
             if (stat == 0) {
@@ -148,39 +261,38 @@ int execute(char** tokens) {
 
             pid_t pid = fork();
             if (pid == 0) {
-                // Redirection
                 int fd_in = -1, fd_out = -1;
-                for (int i = 1; tokens[i] != NULL; i++) {
-                    if (strcmp(tokens[i], "<") == 0) {
-                        fd_in = open(tokens[i + 1], O_RDONLY);
+                for (int j = 1; tokens[j] != NULL; j++) {
+                    if (strcmp(tokens[j], "<") == 0) {
+                        fd_in = open(tokens[j + 1], O_RDONLY);
                         if (fd_in < 0) {
                             perror("open");
                             exit(1);
                         }
                         dup2(fd_in, STDIN_FILENO);
                         close(fd_in);
-                        tokens[i] = NULL;
-                        tokens[i + 1] = NULL;
-                    } else if (strcmp(tokens[i], ">") == 0) {
-                        fd_out = open(tokens[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+                        tokens[j] = NULL;
+                        tokens[j + 1] = NULL;
+                    } else if (strcmp(tokens[j], ">") == 0) {
+                        fd_out = open(tokens[j + 1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
                         if (fd_out < 0) {
                             perror("open");
                             exit(1);
                         }
                         dup2(fd_out, STDOUT_FILENO);
                         close(fd_out);
-                        tokens[i] = NULL;
-                        tokens[i + 1] = NULL;
-                    } else if (strcmp(tokens[i], "2>") == 0) {
-                        fd_out = open(tokens[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+                        tokens[j] = NULL;
+                        tokens[j + 1] = NULL;
+                    } else if (strcmp(tokens[j], "2>") == 0) {
+                        fd_out = open(tokens[j + 1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
                         if (fd_out < 0) {
                             perror("open");
                             exit(1);
                         }
                         dup2(fd_out, STDERR_FILENO);
                         close(fd_out);
-                        tokens[i] = NULL;
-                        tokens[i + 1] = NULL;
+                        tokens[j] = NULL;
+                        tokens[j + 1] = NULL;
                     }
                 }
 
@@ -188,7 +300,22 @@ int execute(char** tokens) {
                 perror("execvp");
                 exit(1);
             } else {
-                wait(NULL);
+                if (!background && !sequential) {
+                    waitpid(pid, NULL, 0);
+                } else if (background) {
+                    pid_t shell_pid = getpid();
+                    if (pid != 0) {
+                  
+                        freopen("/dev/null", "w", stdout);
+                        freopen("/dev/null", "w", stderr);
+                        printf("[%d] %d\n", shell_pid, pid);
+                    }
+                } else if (sequential) {
+                    waitpid(pid, NULL, 0);
+                    if (tokens[i + 1] != NULL) {
+                        execute(tokens + i + 1); // Execute the command after ;
+                    }
+                }
             }
         }
     }
@@ -196,8 +323,11 @@ int execute(char** tokens) {
     return 0;
 }
 
+
 int main() {
-    // Set up signal handlers to ignore signals
+    rl_bind_key('\t', rl_complete);
+    using_history();
+
     signal(SIGTSTP, sigtstp_handler);
     signal(SIGINT, sigint_handler);
     signal(SIGQUIT, sigquit_handler);
@@ -211,23 +341,51 @@ int main() {
 
     printf("\n");
     while (1) {
-        printf("%s", prompt);
+        char* input = readline(prompt);
 
-        char input[MAX_INPUT_SIZE];
-        if (!fgets(input, MAX_INPUT_SIZE, stdin)) {
+        if (!input) {
             printf("\n");
             break;
         }
 
-        char** tokens = tokenize(input);
+        if (input && *input) {
+        
+            if (input[0] == '!') {
+                char* recalled_command = handle_history_recall(input);
+                if (recalled_command != NULL) {
+                    free(input);
+                    input = recalled_command;
+                } else {
+                    free(input);
+                    continue; 
+                }
+            }
 
-        int status = execute(tokens);
+      
+            add_to_history(input);
 
-        free(tokens);
+            char** tokens = tokenize(input);
 
-        if (status) {
-            continue;
+            
+            expand_wildcards(tokens);
+
+            int status = execute(tokens);
+
+            // Free allocated memory
+            for (int i = 0; tokens[i] != NULL; i++) {
+                free(tokens[i]);
+            }
+            free(tokens);
         }
+
+        free(input);
     }
+
+    clear_history();
+
+    for (int i = 0; i < history_count; i++) {
+        free(history[i]);
+    }
+
     return 0;
 }
